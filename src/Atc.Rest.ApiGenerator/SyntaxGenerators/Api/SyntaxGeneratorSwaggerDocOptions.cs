8f126964496648796c65d2fdeb9543b9fe573f4a
Christian Helle <christian.helle@outlook.com>
2021-01-29 11:19:01 +0100

    Update syntax tree text for ConfigureSwaggerDocOptions to depend on IApiVersionDescriptionProvider

--- src/Atc.Rest.ApiGenerator/SyntaxGenerators/Api/SyntaxGeneratorSwaggerDocOptions.cs
+++ src/Atc.Rest.ApiGenerator/SyntaxGenerators/Api/SyntaxGeneratorSwaggerDocOptions.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.CodeAnalysis.CSharp;
+﻿using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.OpenApi.Models;
 
 namespace Atc.Rest.ApiGenerator.SyntaxGenerators.Api
@@ -18,6 +17,12 @@ namespace Atc.Rest.ApiGenerator.SyntaxGenerators.Api
         }
 
         public string GenerateCode()
+            => SyntaxFactory
+                .ParseSyntaxTree(GetSyntaxTreeText())
+                .GetCompilationUnitRoot()
+                .ToFullString();
+
+        private string GetSyntaxTreeText()
         {
             var contactUrl =
                 string.IsNullOrWhiteSpace(document.Info?.Contact?.Url?.ToString())
@@ -27,11 +32,13 @@ namespace Atc.Rest.ApiGenerator.SyntaxGenerators.Api
 
             var licenseUrl =
                 string.IsNullOrWhiteSpace(document.Info?.License?.Url?.ToString())
-                ? string.Empty
-                : $@"
+                    ? string.Empty
+                    : $@"
                         Url = new Uri(""{document.Info?.License?.Url}""),";
 
-            string code = $@"using System;
+            return $@"using System;
+using System.IO;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
@@ -41,35 +48,39 @@ namespace {fullNamespace}
 {{
     public class ConfigureSwaggerDocOptions : IConfigureOptions<SwaggerGenOptions>
     {{
+        private readonly IApiVersionDescriptionProvider provider;
+
+        public ConfigureSwaggerDocOptions(IApiVersionDescriptionProvider provider)
+            => this.provider = provider;
+
         public void Configure(SwaggerGenOptions options)
         {{
-            options.SwaggerDoc(
-                ""{document.Info?.Version}"",
-                new OpenApiInfo
-                {{
-                    Version = ""{document.Info?.Version}"",
-                    Title = ""{document.Info?.Title}"",
-                    Description = @""{document.Info?.Description}"",
-                    Contact = new OpenApiContact()
+            foreach (var version in provider.ApiVersionDescriptions)
+            {{
+                options.SwaggerDoc(
+                    version.GroupName,
+                    new OpenApiInfo
                     {{
-                        Name = ""{document.Info?.Contact?.Name}"",{contactUrl}
-                        Email = ""{document.Info?.Contact?.Email}"",
-                    }},
-                    TermsOfService = new Uri(""{document.Info?.TermsOfService}""),
-                    License = new OpenApiLicense()
-                    {{
-                        Name = ""{document.Info?.License?.Name}"",{licenseUrl}
-                    }},
-                }});
+                        Version = ""{document.Info?.Version}"",
+                        Title = ""{document.Info?.Title}"",
+                        Description = @""{document.Info?.Description}"",
+                        Contact = new OpenApiContact()
+                        {{
+                            Name = ""{document.Info?.Contact?.Name}"",{contactUrl}
+                            Email = ""{document.Info?.Contact?.Email}"",
+                        }},
+                        TermsOfService = new Uri(""{document.Info?.TermsOfService}""),
+                        License = new OpenApiLicense()
+                        {{
+                            Name = ""{document.Info?.License?.Name}"",{licenseUrl}
+                        }},
+                    }});
+            }}
+
+            options.IncludeXmlComments(Path.ChangeExtension(GetType().Assembly.Location, ""xml""));
         }}
     }}
 }}";
-            return SyntaxFactory.ParseSyntaxTree(code)
-                .GetCompilationUnitRoot()
-                .ToFullString()
-                .FormatAutoPropertiesOnOneLine()
-                .FormatPublicPrivateLines()
-                .FormatDoubleLines();
         }
     }
 }
