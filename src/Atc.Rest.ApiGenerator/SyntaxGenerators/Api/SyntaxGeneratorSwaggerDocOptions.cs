f22ca4e6c5f78160d12b6e60f349629f1d165216
Christian Helle <christian.helle@outlook.com>
2021-01-29 01:13:03 +0100

    Introduce SyntaxGeneratorSwaggerDocDocOptions

---
+++ src/Atc.Rest.ApiGenerator/SyntaxGenerators/Api/SyntaxGeneratorSwaggerDocOptions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.OpenApi.Models;
+
+namespace Atc.Rest.ApiGenerator.SyntaxGenerators.Api
+{
+    public class SyntaxGeneratorSwaggerDocDocOptions
+    {
+        private readonly string fullNamespace;
+        private readonly OpenApiDocument document;
+
+        public SyntaxGeneratorSwaggerDocDocOptions(string fullNamespace, OpenApiDocument document)
+        {
+            this.fullNamespace = fullNamespace;
+            this.document = document;
+        }
+
+        public string GenerateCode()
+        {
+            var contactUrl =
+                string.IsNullOrWhiteSpace(document.Info?.Contact?.Url?.ToString())
+                    ? string.Empty
+                    : $@"
+                        Url = new Uri(""{document.Info?.Contact?.Url}""),";
+
+            var licenseUrl =
+                string.IsNullOrWhiteSpace(document.Info?.License?.Url?.ToString())
+                ? string.Empty
+                : $@"
+                        Url = new Uri(""{document.Info?.License?.Url}""),";
+
+            string code = $@"using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace {fullNamespace}
+{{
+    public class ConfigureSwaggerDocOptions : IConfigureOptions<SwaggerGenOptions>
+    {{
+        public void Configure(SwaggerGenOptions options)
+        {{
+            options.SwaggerDoc(
+                ""{document.Info?.Version}"",
+                new OpenApiInfo
+                {{
+                    Version = ""{document.Info?.Version}"",
+                    Title = ""{document.Info?.Title}"",
+                    Description = @""{document.Info?.Description}"",
+                    Contact = new OpenApiContact()
+                    {{
+                        Name = ""{document.Info?.Contact?.Name}"",{contactUrl}
+                        Email = ""{document.Info?.Contact?.Email}"",
+                    }},
+                    TermsOfService = new Uri(""{document.Info?.TermsOfService}""),
+                    License = new OpenApiLicense()
+                    {{
+                        Name = ""{document.Info?.License?.Name}"",{licenseUrl}
+                    }},
+                }});
+        }}
+    }}
+}}";
+            return SyntaxFactory.ParseSyntaxTree(code)
+                .GetCompilationUnitRoot()
+                .ToFullString()
+                .FormatAutoPropertiesOnOneLine()
+                .FormatPublicPrivateLines()
+                .FormatDoubleLines();
+        }
+    }
+}
---
+++ test/Atc.Rest.ApiGenerator.Tests/SyntaxGenerators/SwaggerDocOptionsTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Atc.Rest.ApiGenerator.SyntaxGenerators.Api;
+using Microsoft.OpenApi.Models;
+using Xunit;
+
+namespace Atc.Rest.ApiGenerator.Tests.SyntaxGenerators
+{
+    public class SwaggerDocOptionsTests
+    {
+        [Fact]
+        public void ToCodeAsString_Should_Return_NotNullOrEmpty()
+        {
+            var sut = new SyntaxGeneratorSwaggerDocDocOptions(
+                GetType().Namespace,
+                new OpenApiDocument
+                {
+                    Info = new OpenApiInfo()
+                    {
+                        Contact = new OpenApiContact()
+                        {
+                            Email = "apiteam@swagger.io",
+                        },
+                        Description = @"
+This is a sample Pet Store Server based on the OpenAPI 3.0 specification.  You can find out more about
+    Swagger at [http://swagger.io](http://swagger.io). In the third iteration of the pet store, we've switched to the design first approach!
+    You can now help us improve the API whether it's by making changes to the definition itself or to the code.
+    That way, with time, we can improve the API in general, and expose some of the new features in OAS3.
+
+    Some useful links:
+    - [The Pet Store repository](https://github.com/swagger-api/swagger-petstore)
+    - [The source API definition for the Pet Store](https://github.com/swagger-api/swagger-petstore/blob/master/src/main/resources/openapi.yaml)",
+                        License = new OpenApiLicense
+                        {
+                            Name = "Apache 2.0",
+                            Url = new Uri("http://www.apache.org/licenses/LICENSE-2.0.html"),
+                        },
+                        Title = "Swagger Petstore - OpenAPI 3.0",
+                        Version = "1.0.6",
+                        TermsOfService = new Uri("http://swagger.io/terms/"),
+                    },
+                }
+            );
+
+            Assert.False(string.IsNullOrWhiteSpace(sut.GenerateCode()));
+        }
+    }
+}
