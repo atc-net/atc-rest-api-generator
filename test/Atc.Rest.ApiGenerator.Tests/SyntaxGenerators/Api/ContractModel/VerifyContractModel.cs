33d9208b8b8205aa2c565e557b6a9264dee0dabe
Simon Cropp <simon.cropp@gmail.com>
2021-01-31 11:26:39 +1100

    Update VerifyContractModel.cs

--- test/Atc.Rest.ApiGenerator.Tests/SyntaxGenerators/Api/ContractModel/VerifyContractModel.cs
+++ test/Atc.Rest.ApiGenerator.Tests/SyntaxGenerators/Api/ContractModel/VerifyContractModel.cs
@@ -1,9 +1,7 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
 using System.Threading.Tasks;
 using Atc.Rest.ApiGenerator.SyntaxGenerators.Api;
 using Atc.Rest.ApiGenerator.Tests.Helpers;
