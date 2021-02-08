bbc7dfaa8e441bb5c7c2af3359e94419db7294a3
Thomas Malowanczyk <tkm@delegate.dk>
2021-02-08 15:57:39 +0100

    Add test for Endpoint Controllers generator

---
+++ test/Atc.Rest.ApiGenerator.Tests/SyntaxGenerators/Api/EndpointControllersTest.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Atc.Rest.ApiGenerator.Helpers;
+using Atc.Rest.ApiGenerator.Models;
+using Atc.Rest.ApiGenerator.SyntaxGenerators;
+using Atc.Rest.ApiGenerator.SyntaxGenerators.Api;
+using Atc.Rest.ApiGenerator.Tests.Helpers.CodeGenerator;
+using VerifyXunit;
+using Xunit;
+
+namespace Atc.Rest.ApiGenerator.Tests.SyntaxGenerators
+{
+    [UsesVerify]
+    public class EndpointControllersTest : SyntaxGeneratorTestBase
+    {
+        public static IEnumerable<object[]> YamlFiles { get; } = AllFiles
+            .Where(x => x.FilePath.Contains("EndpointControllers", System.StringComparison.Ordinal))
+            .Select(x => new object[] { x });
+
+        protected override ISyntaxCodeGenerator CreateApiGenerator(ApiProjectOptions apiProject)
+        {
+            // Verify spec file suported for unit test
+            Assert.Single(apiProject.BasePathSegmentNames);
+            var segmentName = apiProject.BasePathSegmentNames.First();
+            var operationSchemaMappings = OpenApiOperationSchemaMapHelper.CollectMappings(apiProject.Document);
+
+            // Construct SUT
+            return new SyntaxGeneratorEndpointControllers(apiProject, operationSchemaMappings, segmentName);
+        }
+
+        [Theory(DisplayName = "Contract Controllers")]
+        [MemberData(nameof(YamlFiles))]
+        public Task ExecuteGeneratorTest(YamlSpecFile specFile) => ExecuteTest(specFile);
+    }
+}
