772cfe4d9892e5e4e78014368223891e2ebeab08
Thomas Malowanczyk <tkm@delegate.dk>
2021-02-08 15:56:27 +0100

    Add test for Contract Interface generator

---
+++ test/Atc.Rest.ApiGenerator.Tests/SyntaxGenerators/Api/ContractInterfaceTest.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Atc.Rest.ApiGenerator.Models;
+using Atc.Rest.ApiGenerator.SyntaxGenerators;
+using Atc.Rest.ApiGenerator.SyntaxGenerators.Api;
+using Atc.Rest.ApiGenerator.Tests.Helpers.CodeGenerator;
+using Microsoft.OpenApi.Models;
+using VerifyXunit;
+using Xunit;
+
+namespace Atc.Rest.ApiGenerator.Tests.SyntaxGenerators
+{
+    [UsesVerify]
+    public class ContractInterfaceTest : SyntaxGeneratorTestBase
+    {
+        public static IEnumerable<object[]> YamlFiles { get; } = AllFiles
+            .Where(x => x.FilePath.Contains("ContractInterface", System.StringComparison.Ordinal))
+            .Select(x => new object[] { x });
+
+        protected override ISyntaxCodeGenerator CreateApiGenerator(ApiProjectOptions apiProject)
+        {
+            // Verify spec file suported for unit test
+            Assert.Single(apiProject.Document.Paths);
+            var urlPath = apiProject.Document.Paths.First();
+            Assert.False(urlPath.IsPathStartingSegmentName(FocusOnSecment));
+            Assert.Single(urlPath.Value.Operations);
+            var urlOperation = urlPath.Value.Operations.First();
+
+            // Construct SUT
+            return new SyntaxGeneratorContractInterface(
+                        apiProject,
+                        urlPath.Value.Parameters,
+                        urlOperation.Key,
+                        urlOperation.Value,
+                        FocusOnSecment,
+                        urlPath.Value.HasParameters() || urlOperation.Value.HasParametersOrRequestBody());
+        }
+
+        [Theory(DisplayName = "Contract Interface")]
+        [MemberData(nameof(YamlFiles))]
+        public Task ExecuteGeneratorTest(YamlSpecFile specFile) => ExecuteTest(specFile);
+    }
+}
