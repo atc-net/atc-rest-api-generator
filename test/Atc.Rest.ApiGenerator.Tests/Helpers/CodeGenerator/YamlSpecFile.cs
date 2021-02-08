675880865285541cb9594673f5cfceeab7c33577
Thomas Malowanczyk <tkm@delegate.dk>
2021-02-08 15:56:06 +0100

    Setup base class for snaptshot testing with Verify

---
+++ test/Atc.Rest.ApiGenerator.Tests/Helpers/CodeGenerator/YamlSpecFile.cs
@@ -0,0 +1,29 @@
+using System.IO;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Atc.Rest.ApiGenerator.Tests.Helpers.CodeGenerator
+{
+    public class YamlSpecFile : IXunitSerializable
+    {
+        public FileInfo FileInfo { get; private set; }
+
+        public string FilePath => FileInfo.FullName;
+
+        public string FileName => FileInfo.Name;
+
+        public string DirectoryName => FileInfo.DirectoryName;
+
+        public YamlSpecFile() { }
+
+        public YamlSpecFile(FileInfo fileInfo) => this.FileInfo = fileInfo;
+
+        public void Deserialize(IXunitSerializationInfo info) => FileInfo = new FileInfo(info.GetValue<string>("fileInfo"));
+
+        public void Serialize(IXunitSerializationInfo info) => info.AddValue("fileInfo", FileInfo.FullName);
+
+        public Task<string> LoadFileContentAsync() => FileInfo.OpenText().ReadToEndAsync();
+
+        public override string ToString() => this.FileName;
+    }
+}
---
+++ test/Atc.Rest.ApiGenerator.Tests/SyntaxGenerators/Api/SyntaxGeneratorTestBase.cs
@@ -0,0 +1,98 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Atc.Rest.ApiGenerator.Models;
+using Atc.Rest.ApiGenerator.SyntaxGenerators;
+using Atc.Rest.ApiGenerator.Tests.Helpers.CodeGenerator;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Readers;
+using VerifyTests;
+using VerifyXunit;
+
+namespace Atc.Rest.ApiGenerator.Tests.SyntaxGenerators
+{
+    public abstract class SyntaxGeneratorTestBase
+    {
+        protected static readonly string FocusOnSecment = "Test";
+        private static readonly string ProjectPrefix = "TestProject";
+        private static readonly string ProjectSuffix = "AtcTest";
+
+        protected static IReadOnlyList<YamlSpecFile> AllFiles { get; } = GetYamlFiles();
+
+        protected abstract ISyntaxCodeGenerator CreateApiGenerator(ApiProjectOptions apiProject);
+
+        protected static Task VerifyGeneratedCode(string generatedCode, VerifySettings verifySettings)
+        {
+            return Verifier.Verify(generatedCode, verifySettings);
+        }
+
+        protected async Task ExecuteTest(YamlSpecFile specFile)
+        {
+            // Arrange
+            var apiProject = await CreateApiProjectAsync(specFile);
+            var verifySettings = CreateVerifySettings(specFile, apiProject);
+
+            var sut = CreateApiGenerator(apiProject);
+
+            // Act
+            var generatedCode = sut.ToCodeAsString();
+
+            // Assert
+            await VerifyGeneratedCode(generatedCode, verifySettings);
+        }
+
+        private VerifySettings CreateVerifySettings(YamlSpecFile yamlFile, ApiProjectOptions apiOptions)
+        {
+            var settings = new VerifySettings();
+            settings.UseDirectory(yamlFile.DirectoryName);
+            settings.UseFileName(yamlFile.FileName);
+            settings.UseExtension("cs");
+            settings.AddScrubber(input => input.Replace(apiOptions.ToolVersion.ToString(), "x.x.x.x"));
+            return settings;
+        }
+
+        private static IReadOnlyList<YamlSpecFile> GetYamlFiles([CallerFilePath] string sourceFilePath = "")
+        {
+            var directory = Path.GetDirectoryName(sourceFilePath);
+            return Directory.EnumerateFiles(directory, "*.yaml", SearchOption.AllDirectories)
+                .Select(x => new YamlSpecFile(new FileInfo(x)))
+                .ToArray();
+        }
+
+        private async Task<ApiProjectOptions> CreateApiProjectAsync(YamlSpecFile specFile)
+        {
+            var spec = await specFile.LoadFileContentAsync();
+            var document = GenerateApiDocument(spec);
+
+            return new ApiProjectOptions(
+                new DirectoryInfo("resources"),
+                null,
+                document,
+                new FileInfo("resources/dummySpec.yaml"),
+                ProjectPrefix,
+                ProjectSuffix,
+                new Models.ApiOptions.ApiOptions(),
+                null);
+        }
+
+        private OpenApiDocument GenerateApiDocument(string spec)
+        {
+            using var specStream = GenerateStreamFromString(spec);
+            var openApiStreamReader = new OpenApiStreamReader();
+            var openApiDocument = openApiStreamReader.Read(specStream, out var diagnostic);
+            return openApiDocument;
+        }
+
+        public Stream GenerateStreamFromString(string s)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+    }
+}
