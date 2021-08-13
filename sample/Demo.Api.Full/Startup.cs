315f4daff7c6593751e8ee2d8b1ad5761f9c5a62
Per Kops <pko@delegate.dk>
2021-08-13 11:57:02 +0200

    Remove test-code

--- sample/Demo.Api.Full/Startup.cs
+++ sample/Demo.Api.Full/Startup.cs
@@ -1,7 +1,4 @@
-using System;
-using System.IO;
 using System.Reflection;
-using System.Threading.Tasks;
 using Atc;
 using Atc.Rest.Extended.Options;
 using Atc.Rest.Options;
@@ -10,7 +7,6 @@ using Demo.Api.Generated;
 using Demo.Domain;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -65,39 +61,11 @@ namespace Demo.Api.Full
 
             services.ConfigureOptions<ConfigureSwaggerOptions>();
             services.AddRestApi<Startup>(restApiOptions, Configuration);
-
-            ////services.AddControllers(options =>
-            ////    options.InputFormatters.Add(new ByteArrayInputFormatter()));
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.ConfigureRestApi(env, restApiOptions);
-        }
-    }
-
-#pragma warning disable MA0048 // File name must match type name
-#pragma warning disable SA1402 // File may only contain a single type
-    public class ByteArrayInputFormatter : InputFormatter
-#pragma warning restore SA1402 // File may only contain a single type
-#pragma warning restore MA0048 // File name must match type name
-    {
-        public ByteArrayInputFormatter()
-        {
-            SupportedMediaTypes.Add(Microsoft.Net.Http.Headers.MediaTypeHeaderValue.Parse("application/octet-stream"));
-        }
-
-        protected override bool CanReadType(Type type)
-        {
-            return type == typeof(byte[]);
-        }
-
-        public override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context)
-        {
-            var stream = new MemoryStream();
-            await context.HttpContext.Request.Body.CopyToAsync(stream);
-            ////return await InputFormatterResult.SuccessAsync(stream.ToArray());
-            return await InputFormatterResult.SuccessAsync(stream.ToString());
         }
     }
 }
