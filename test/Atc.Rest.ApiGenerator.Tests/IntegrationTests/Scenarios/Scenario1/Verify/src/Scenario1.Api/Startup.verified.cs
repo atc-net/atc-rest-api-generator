e68b93db441258de6497f2e7b742e719f1f34ec0
lupusbytes <bertram@lupusbytes.com>
2021-09-22 12:15:46 +0200

    Add Scenario1 for validate and build integration test

---
+++ test/Atc.Rest.ApiGenerator.Tests/IntegrationTests/Scenarios/Scenario1/Verify/src/Scenario1.Api/Startup.verified.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using Atc.Rest.Extended.Options;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Scenario1.Api.Generated;
+using Scenario1.Domain;
+
+namespace Scenario1.Api
+{
+    public class Startup
+    {
+        private readonly RestApiExtendedOptions restApiOptions;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            restApiOptions = new RestApiExtendedOptions();
+            restApiOptions.AddAssemblyPairs(Assembly.GetAssembly(typeof(ApiRegistration)), Assembly.GetAssembly(typeof(DomainRegistration)));
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.ConfigureOptions<ConfigureSwaggerDocOptions>();
+
+            services.AddRestApi<Startup>(restApiOptions, Configuration);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.ConfigureRestApi(env, restApiOptions);
+        }
+    }
+}
