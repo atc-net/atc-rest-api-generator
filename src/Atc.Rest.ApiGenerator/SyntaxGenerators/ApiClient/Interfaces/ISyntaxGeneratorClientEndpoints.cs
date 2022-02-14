c97fe6f9ac9b0346b3fc86b2ba1ede0ffd3a94d3
David Kallesen <david@lmdk.dk>
2022-02-14 23:50:03 +0100

    Fix CA1040

--- src/Atc.Rest.ApiGenerator/SyntaxGenerators/ApiClient/Interfaces/ISyntaxGeneratorClientEndpoints.cs
+++ src/Atc.Rest.ApiGenerator/SyntaxGenerators/ApiClient/Interfaces/ISyntaxGeneratorClientEndpoints.cs
@@ -2,4 +2,11 @@ namespace Atc.Rest.ApiGenerator.SyntaxGenerators.ApiClient.Interfaces;
 
 public interface ISyntaxGeneratorClientEndpoints
 {
+    ApiProjectOptions ApiProjectOptions { get; }
+
+    List<ApiOperationSchemaMap> OperationSchemaMappings { get; }
+
+    string FocusOnSegmentName { get; }
+
+    List<SyntaxGeneratorClientEndpoint> GenerateSyntaxTrees();
 }
