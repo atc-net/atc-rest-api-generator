5581b1dc29feb011f6b406114ca77eb90e66b8f7
David Kallesen <david@lmdk.dk>
2022-03-11 16:04:30 +0100

    ci: Add TODO and comment out RuleFor, until https://github.com/FluentValidation/FluentValidation/issues/1913 is fixed

--- sample/src/Demo.Domain/Validators/Users/PostUserParametersValidator.cs
+++ sample/src/Demo.Domain/Validators/Users/PostUserParametersValidator.cs
@@ -7,16 +7,18 @@ namespace Demo.Domain.Validators.Users
     {
         public PostUserParametersValidator()
         {
-            RuleFor(x => x.Request.FirstName)
-                .NotNull()
-                .Length(2, 10)
-                .Matches(@"^[A-Z]").WithMessage(x => $"{nameof(x.Request.FirstName)} has to start with an uppercase letter");
+            // TODO: Waiting on https://github.com/FluentValidation/FluentValidation/issues/1913
 
-            RuleFor(x => x.Request.LastName)
-                .NotNull()
-                .Length(2, 30)
-                .Matches(@"^[A-Z]").WithMessage(x => $"{nameof(x.Request.LastName)} has to start with an uppercase letter")
-                .NotEqual(x => x.Request.FirstName);
+            ////RuleFor(x => x.Request.FirstName)
+            ////    .NotNull()
+            ////    .Length(2, 10)
+            ////    .Matches(@"^[A-Z]").WithMessage(x => $"{nameof(x.Request.FirstName)} has to start with an uppercase letter");
+
+            ////RuleFor(x => x.Request.LastName)
+            ////    .NotNull()
+            ////    .Length(2, 30)
+            ////    .Matches(@"^[A-Z]").WithMessage(x => $"{nameof(x.Request.LastName)} has to start with an uppercase letter")
+            ////    .NotEqual(x => x.Request.FirstName);
         }
     }
 }
