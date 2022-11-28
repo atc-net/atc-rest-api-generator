45d19ff663451c6bb72a19014585801a033f0ed6
Per Kops <pko@delegate.dk>
2022-11-28 12:29:33 +0100

    fix: Extracting method to IApiSpecificationContentReader

--- src/Atc.Rest.ApiGenerator.Framework/IApiSpecificationContentReader.cs
+++ src/Atc.Rest.ApiGenerator.Framework/IApiSpecificationContentReader.cs
@@ -2,4 +2,7 @@ namespace Atc.Rest.ApiGenerator.Framework;
 
 public interface IApiSpecificationContentReader
 {
+    OpenApiDocumentContainer CombineAndGetApiDocumentContainer(
+        ILogger logger,
+        string specificationPath);
 }
