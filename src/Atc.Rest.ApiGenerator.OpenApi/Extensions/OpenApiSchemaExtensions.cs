862d594510dda5a16d90934052af4856990ca791
David Kallesen <david@lmdk.dk>
2022-12-21 18:51:40 +0100

    feat: Add OpenApiSchemaExtensions: GetParameterLocationType and GetRequestBodySummaryDescription

--- src/Atc.Rest.ApiGenerator.OpenApi/Extensions/OpenApiSchemaExtensions.cs
+++ src/Atc.Rest.ApiGenerator.OpenApi/Extensions/OpenApiSchemaExtensions.cs
@@ -33,4 +33,46 @@ public static class OpenApiSchemaExtensions
         => apiSchema.AllOf.Count == 2 &&
            (NameConstants.Pagination.Equals(apiSchema.AllOf[0].Reference?.Id, StringComparison.OrdinalIgnoreCase) ||
             NameConstants.Pagination.Equals(apiSchema.AllOf[1].Reference?.Id, StringComparison.OrdinalIgnoreCase));
+
+    public static ParameterLocationType GetParameterLocationType(
+        this OpenApiSchema apiSchema,
+        ParameterLocationType defaultParameterLocationType = ParameterLocationType.Body)
+    {
+        var parameterLocationType = defaultParameterLocationType;
+        if (apiSchema.IsTypeArray())
+        {
+            if (apiSchema.HasItemsWithFormatTypeBinary())
+            {
+                parameterLocationType = ParameterLocationType.Form;
+            }
+        }
+        else
+        {
+            if (apiSchema.HasAnyPropertiesWithFormatTypeBinary() ||
+                apiSchema.HasAnyPropertiesAsArrayWithFormatTypeBinary())
+            {
+                parameterLocationType = ParameterLocationType.Form;
+            }
+        }
+
+        return parameterLocationType;
+    }
+
+    public static string GetRequestBodySummaryDescription(
+        this OpenApiSchema apiSchema)
+    {
+        var result = apiSchema.Description;
+
+        if (string.IsNullOrEmpty(result))
+        {
+            return ContentGeneratorConstants.UndefinedDescription;
+        }
+
+        if (!result.EndsWith('.'))
+        {
+            result += ".";
+        }
+
+        return result;
+    }
 }
