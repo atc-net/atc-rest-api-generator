bff91cc6375228e0fa48cea052f82ba6ee6e5d2c
David Kallesen <david@lmdk.dk>
2023-01-19 10:21:41 +0100

    chore: Map Emojis to use Console.EmojiConstants

--- src/Atc.Rest.ApiGenerator.CodingRules/CodingRulesConstants.cs
+++ src/Atc.Rest.ApiGenerator.CodingRules/CodingRulesConstants.cs
@@ -2,11 +2,10 @@ namespace Atc.Rest.ApiGenerator.CodingRules;
 
 public static class CodingRulesConstants
 {
-    public const string AreaCodingRules = "📐";
-    public const string AreaEditorConfig = "🐭";
-    public const string AreaDirectoryBuildProps = "🔨";
+    public const string AreaCodingRules = Console.EmojiConstants.TriangularRuler;
+    public const string AreaEditorConfig = Console.EmojiConstants.MouseFace;
+    public const string AreaDirectoryBuildProps = Console.EmojiConstants.Hammer;
 
-    public const string LogFileCreated = "🟢"; // Green
-
-    public const string LogError = "🔴"; // Red
+    public const string LogFileCreated = Console.EmojiConstants.GreenSquare;
+    public const string LogError = Console.EmojiConstants.RedCircle;
 }
