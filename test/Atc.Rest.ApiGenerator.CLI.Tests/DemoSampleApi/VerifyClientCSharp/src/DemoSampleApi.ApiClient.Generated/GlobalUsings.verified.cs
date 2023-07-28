84fa17791be499fbdb5c0ae2b489b7c00aac0146
Per Kops <pko@delegate.dk>
2023-07-28 15:30:31 +0200

    chore: Updating verification tests to match new client c sharp global using file content

--- test/Atc.Rest.ApiGenerator.CLI.Tests/DemoSampleApi/VerifyClientCSharp/src/DemoSampleApi.ApiClient.Generated/GlobalUsings.verified.cs
+++ test/Atc.Rest.ApiGenerator.CLI.Tests/DemoSampleApi/VerifyClientCSharp/src/DemoSampleApi.ApiClient.Generated/GlobalUsings.verified.cs
@@ -1,16 +1,9 @@
 global using System.CodeDom.Compiler;
-global using System.Collections.Generic;
 global using System.ComponentModel.DataAnnotations;
-global using System.Diagnostics.CodeAnalysis;
-global using System.Linq;
 global using System.Net;
-global using System.Net.Http;
-global using System.Threading;
-global using System.Threading.Tasks;
 
 global using Atc.Rest.Client;
 global using Atc.Rest.Client.Builder;
-global using Atc.Rest.Results;
 
 global using DemoSampleApi.ApiClient.Generated.Contracts;
 
--- test/Atc.Rest.ApiGenerator.CLI.Tests/GenericPaginationApi/VerifyClientCSharp/src/GenericPaginationApi.ApiClient.Generated/GlobalUsings.verified.cs
+++ test/Atc.Rest.ApiGenerator.CLI.Tests/GenericPaginationApi/VerifyClientCSharp/src/GenericPaginationApi.ApiClient.Generated/GlobalUsings.verified.cs
@@ -1,18 +1,10 @@
 global using System.CodeDom.Compiler;
-global using System.Collections.Generic;
 global using System.ComponentModel.DataAnnotations;
-global using System.Diagnostics.CodeAnalysis;
-global using System.Linq;
 global using System.Net;
-global using System.Net.Http;
-global using System.Threading;
-global using System.Threading.Tasks;
 
 global using Atc.Rest.Client;
 global using Atc.Rest.Client.Builder;
-global using Atc.Rest.Results;
 
 global using GenericPaginationApi.ApiClient.Generated.Contracts;
 
-global using Microsoft.AspNetCore.Http;
 global using Microsoft.AspNetCore.Mvc;
