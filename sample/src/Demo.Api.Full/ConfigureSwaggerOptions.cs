b0318282c15223fe9c3cea77918c8a8b89413afe
Per Kops <pko@delegate.dk>
2023-07-28 15:36:07 +0200

    fix: Cleaning up ConfigureSwaggerOptions in Demo.Api.Full

--- sample/src/Demo.Api.Full/ConfigureSwaggerOptions.cs
+++ sample/src/Demo.Api.Full/ConfigureSwaggerOptions.cs
@@ -1,85 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using Atc.Rest.Extended.Options;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
-using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.SwaggerGen;
 using Swashbuckle.AspNetCore.SwaggerUI;
 
-namespace Demo.Api.Full
+namespace Demo.Api.Full;
+
+public class ConfigureSwaggerOptions :
+    IConfigureOptions<SwaggerGenOptions>,
+    IConfigureOptions<SwaggerUIOptions>
 {
-     public class ConfigureSwaggerOptions :
-        IConfigureOptions<SwaggerGenOptions>,
-        IConfigureOptions<SwaggerUIOptions>
+    private readonly RestApiExtendedOptions apiOptions;
+    private readonly IWebHostEnvironment env;
+
+    public ConfigureSwaggerOptions(
+        RestApiExtendedOptions options,
+        IWebHostEnvironment env)
     {
-        private readonly RestApiExtendedOptions apiOptions;
-        private readonly IWebHostEnvironment env;
+        this.apiOptions = options ?? throw new ArgumentNullException(nameof(options));
+        this.env = env ?? throw new ArgumentNullException(nameof(env));
+    }
 
-        public ConfigureSwaggerOptions(
-            RestApiExtendedOptions options,
-            IWebHostEnvironment env)
+    public void Configure(SwaggerGenOptions options)
+    {
+        if (!env.IsDevelopment())
         {
-            this.apiOptions = options ?? throw new ArgumentNullException(nameof(options));
-            this.env = env ?? throw new ArgumentNullException(nameof(env));
+            return;
         }
 
-        public void Configure(SwaggerGenOptions options)
+        var authority = $"{apiOptions.Authorization!.Instance}/{apiOptions.Authorization.TenantId}";
+        var authorizationUrl = $"{authority}/oauth2/v2.0/authorize";
+        var tokenUrl = $"{authority}/oauth2/v2.0/token";
+        var authFlow = new OpenApiOAuthFlow
         {
-            if (!env.IsDevelopment())
+            TokenUrl = new Uri(tokenUrl),
+            AuthorizationUrl = new Uri(authorizationUrl),
+            Scopes = new Dictionary<string, string>
             {
-                return;
-            }
+                { $"{apiOptions.Authorization.Audience}/.default", "Default" },
+            },
+        };
 
-            var authority = $"{apiOptions.Authorization.Instance}/{apiOptions.Authorization.TenantId}";
-            var authorizationUrl = $"{authority}/oauth2/v2.0/authorize";
-            var tokenUrl = $"{authority}/oauth2/v2.0/token";
-            var authFlow = new OpenApiOAuthFlow
+        options.AddSecurityDefinition(
+            SecuritySchemeType.OAuth2.ToString(),
+            new OpenApiSecurityScheme
             {
-                TokenUrl = new Uri(tokenUrl),
-                AuthorizationUrl = new Uri(authorizationUrl),
-                Scopes = new Dictionary<string, string>
+                Type = SecuritySchemeType.OAuth2,
+                Flows = new OpenApiOAuthFlows
                 {
-                    { $"{apiOptions.Authorization.Audience}/.default", "Default" },
+                    Implicit = authFlow,
+                    AuthorizationCode = authFlow,
                 },
-            };
+            });
 
-            options.AddSecurityDefinition(
-                SecuritySchemeType.OAuth2.ToString(),
-                new OpenApiSecurityScheme
+        options.AddSecurityRequirement(
+            new OpenApiSecurityRequirement
+            {
                 {
-                    Type = SecuritySchemeType.OAuth2,
-                    Flows = new OpenApiOAuthFlows
+                    new OpenApiSecurityScheme
                     {
-                        Implicit = authFlow,
-                        AuthorizationCode = authFlow,
-                    },
-                });
-
-            options.AddSecurityRequirement(
-                new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
+                        Reference = new OpenApiReference
                         {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = SecuritySchemeType.OAuth2.ToString(),
-                            },
+                            Type = ReferenceType.SecurityScheme,
+                            Id = SecuritySchemeType.OAuth2.ToString(),
                         },
-                        Array.Empty<string>()
                     },
-                });
-        }
+                    Array.Empty<string>()
+                },
+            });
+    }
 
-        public void Configure(SwaggerUIOptions options)
-        {
-            options.OAuthClientId(apiOptions.Authorization.ClientId);
-            options.OAuthUsePkce();
-        }
+    public void Configure(SwaggerUIOptions options)
+    {
+        options.OAuthClientId(apiOptions.Authorization!.ClientId);
+        options.OAuthUsePkce();
     }
 }
