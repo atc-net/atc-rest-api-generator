2cb62a5e248666cb9e9e10161d011fcfefe58de2
Per Kops <pko@delegate.dk>
2023-08-16 14:54:26 +0200

    feat: Implemented GenerateParameters for MinimalApi in ServerApiGenerator

---
+++ src/Atc.Rest.ApiGenerator.Framework.Minimal/GlobalUsings.cs
@@ -0,0 +1,10 @@
+global using System.ComponentModel.DataAnnotations;
+global using System.Text;
+global using Atc.CodeDocumentation.CodeComment;
+global using Atc.CodeGeneration.CSharp.Content;
+global using Atc.CodeGeneration.CSharp.Content.Factories;
+global using Atc.CodeGeneration.CSharp.Content.Generators;
+global using Atc.CodeGeneration.CSharp.Extensions;
+global using Atc.Rest.ApiGenerator.Framework.ContentGenerators;
+global using Atc.Rest.ApiGenerator.Framework.Contracts;
+global using Atc.Rest.ApiGenerator.Framework.Contracts.ContentGeneratorsParameters.Server;
