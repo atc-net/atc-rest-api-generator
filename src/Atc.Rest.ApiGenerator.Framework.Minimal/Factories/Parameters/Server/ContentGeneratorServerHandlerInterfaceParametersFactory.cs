a8f2cc2b33efa011c9993a395698f8c254871764
Per Kops <pko@delegate.dk>
2023-08-18 12:58:20 +0200

    feat: Working on generating proper result response types in Interface for handlers in ContentGeneratorServerHandlerInterfaceParametersFactory

--- src/Atc.Rest.ApiGenerator.Framework.Minimal/Factories/Parameters/Server/ContentGeneratorServerHandlerInterfaceParametersFactory.cs
+++ src/Atc.Rest.ApiGenerator.Framework.Minimal/Factories/Parameters/Server/ContentGeneratorServerHandlerInterfaceParametersFactory.cs
@@ -9,6 +9,8 @@ public static class ContentGeneratorServerHandlerInterfaceParametersFactory
         OpenApiPathItem openApiPath,
         OpenApiOperation openApiOperation)
     {
+        ArgumentNullException.ThrowIfNull(openApiOperation);
+
         var operationName = openApiOperation.GetOperationName();
 
         var methodParametersAttributes = new Dictionary<string, string>(StringComparer.Ordinal);
@@ -39,20 +41,7 @@ public static class ContentGeneratorServerHandlerInterfaceParametersFactory
                 Name: "cancellationToken",
                 DefaultValue: "default"));
 
-        /*
-     Task<CreateUserResult> ExecuteAsync(
-        CreateUserParameters parameters,
-        CancellationToken cancellationToken = default);
- */
-
-        /*
-         public interface ICreateUserHandler
-        {
-            Task<Results<CreatedAtRoute, BadRequest<string>, Conflict<string>>> ExecuteAsync(
-                CreateUserParameters parameters,
-                CancellationToken cancellationToken = default);
-        }
-         */
+        var returnTypeName = ConstructReturnTypeName(openApiOperation.Responses);
 
         var methodParameters = new List<MethodParameters>
         {
@@ -67,7 +56,7 @@ public static class ContentGeneratorServerHandlerInterfaceParametersFactory
                     @return: null),
                 Attributes: null,
                 AccessModifier: AccessModifiers.None,
-                ReturnTypeName: $"hest{operationName}{ContentGeneratorConstants.Results}",
+                ReturnTypeName: returnTypeName,
                 ReturnGenericTypeName: "Task",
                 Name: "ExecuteAsync",
                 Parameters: methodParametersParameters,
@@ -86,5 +75,158 @@ public static class ContentGeneratorServerHandlerInterfaceParametersFactory
             InheritedInterfaceTypeName: null,
             Properties: null,
             Methods: methodParameters);
+    }
+
+    private static string ConstructReturnTypeName(
+        OpenApiResponses responses)
+    {
+        var responseReturnTypes = ExtractResponseReturnTypes(responses);
+
+        if (responseReturnTypes.Count == 1)
+        {
+            var (httpStatusCode, returnTypeName) = responseReturnTypes[0];
+            var responseReturnType = httpStatusCode.ToNormalizedString();
+
+            return returnTypeName is null
+                ? responseReturnType
+                : $"{responseReturnType}<{returnTypeName}>";
+        }
+
+        var result = new List<string>();
+        foreach (var (httpStatusCode, returnTypeName) in responseReturnTypes)
+        {
+            result.Add(returnTypeName is null
+                ? httpStatusCode.ToNormalizedString()
+                : $"{httpStatusCode.ToNormalizedString()}<{returnTypeName}>");
+        }
+
+        return $"Results<{string.Join(", ", result)}>";
+    }
+
+    private static List<(HttpStatusCode HttpStatusCode, string? ReturnTypeName)> ExtractResponseReturnTypes(
+        OpenApiResponses responses)
+    {
+        var result = new List<(HttpStatusCode HttpStatusCode, string? ReturnTypeName)>();
+
+        foreach (var response in responses.OrderBy(x => x.Key, StringComparer.Ordinal))
+        {
+            if (!Enum.TryParse(typeof(HttpStatusCode), response.Key, out var parsedType))
+            {
+                continue;
+            }
+
+            var httpStatusCode = parsedType is HttpStatusCode code ? code : 0;
+
+            var isList = responses.IsSchemaTypeArrayForStatusCode(httpStatusCode);
+            var modelName = responses.GetModelNameForStatusCode(httpStatusCode);
+
+            // TODO: IsShared..
+            ////var isShared = apiOperationSchemaMappings.IsShared(modelName);
+            ////modelName = OpenApiDocumentSchemaModelNameResolver.EnsureModelNameWithNamespaceIfNeeded(projectName, apiGroupName, modelName, isShared);
+
+            switch (httpStatusCode)
+            {
+                case HttpStatusCode.OK:
+                case HttpStatusCode.Created:
+                    string? typeResponseName = null;
+
+                    var dataType = modelName;
+
+                    var useBinaryResponse = responses.IsSchemaUsingBinaryFormatForOkResponse();
+                    if (useBinaryResponse)
+                    {
+                        typeResponseName = "byte[]";
+                    }
+                    else
+                    {
+                        if (string.IsNullOrEmpty(modelName))
+                        {
+                            var schema = responses.GetSchemaForStatusCode(httpStatusCode);
+                            if (schema is not null)
+                            {
+                                if (schema.IsSimpleDataType())
+                                {
+                                    dataType = schema.GetDataType();
+                                }
+                                else if (schema.HasArrayItemsWithSimpleDataType())
+                                {
+                                    dataType = schema.GetSimpleDataTypeFromArray();
+                                }
+                                else if (schema.HasPaginationItemsWithSimpleDataType())
+                                {
+                                    dataType = schema.GetSimpleDataTypeFromPagination();
+                                }
+                                else if (schema.IsTypeCustomPagination())
+                                {
+                                    var customPaginationSchema = schema.GetCustomPaginationSchema();
+                                    var customPaginationItemsSchema = schema.GetCustomPaginationItemsSchema();
+                                    if (customPaginationSchema is not null &&
+                                        customPaginationItemsSchema is not null)
+                                    {
+                                        var genericDataTypeName = customPaginationSchema.GetDataType();
+                                        if (customPaginationItemsSchema.Items.IsSimpleDataType())
+                                        {
+                                            dataType = customPaginationItemsSchema.GetDataType();
+                                            typeResponseName = $"{genericDataTypeName}<{dataType}>";
+                                        }
+                                        else
+                                        {
+                                            dataType = customPaginationItemsSchema.GetModelName();
+                                            ////dataType = OpenApiDocumentSchemaModelNameResolver.EnsureModelNameWithNamespaceIfNeeded(projectName, apiGroupName, dataType, isShared); // TODO: FIx isshared
+                                            typeResponseName = $"{genericDataTypeName}<{dataType}>";
+                                        }
+                                    }
+                                }
+                            }
+
+                            if (string.IsNullOrEmpty(dataType))
+                            {
+                                dataType = "string";
+                            }
+                        }
+
+                        if (string.IsNullOrEmpty(typeResponseName))
+                        {
+                            if (isList)
+                            {
+                                typeResponseName = $"IEnumerable<{dataType}>";
+                            }
+                            else
+                            {
+                                var isPagination = responses.IsSchemaTypePaginationForStatusCode(httpStatusCode);
+                                typeResponseName = isPagination
+                                    ? $"{NameConstants.Pagination}<{dataType}>"
+                                    : dataType;
+                            }
+                        }
+                    }
+
+                    result.Add((httpStatusCode, typeResponseName));
+
+                    break;
+                case HttpStatusCode.NotModified:
+                case HttpStatusCode.Accepted:
+                case HttpStatusCode.NoContent:
+                case HttpStatusCode.Unauthorized:
+                case HttpStatusCode.Forbidden:
+                    result.Add((httpStatusCode, null));
+                    break;
+                case HttpStatusCode.BadRequest:
+                case HttpStatusCode.NotFound:
+                case HttpStatusCode.MethodNotAllowed:
+                case HttpStatusCode.Conflict:
+                case HttpStatusCode.InternalServerError:
+                case HttpStatusCode.NotImplemented:
+                case HttpStatusCode.BadGateway:
+                case HttpStatusCode.ServiceUnavailable:
+                case HttpStatusCode.GatewayTimeout:
+                    result.Add((httpStatusCode, "string"));
+                    break;
+                default:
+                    throw new NotImplementedException($"ResponseType for {(int)httpStatusCode} - {httpStatusCode} is missing.");
+            }
+        }
+
+        return result;
     }
 }
