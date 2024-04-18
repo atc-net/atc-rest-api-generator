d7d9996626a6ae6458e07762743e694551dfe5b9
David Kallesen <david@lmdk.dk>
2024-04-18 03:06:23 +0200

    feat: Add PublicStaticClass

--- src/Atc.CodeGeneration.CSharp/Content/AccessModifiers.cs
+++ src/Atc.CodeGeneration.CSharp/Content/AccessModifiers.cs
@@ -19,6 +19,9 @@ public enum AccessModifiers
     [Description("public class")]
     PublicClass,
 
+    [Description("public static class")]
+    PublicStaticClass,
+
     [Description("public record")]
     PublicRecord,
 
