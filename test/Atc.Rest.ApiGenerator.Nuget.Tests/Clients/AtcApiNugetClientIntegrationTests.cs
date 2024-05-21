08165058cc9654afed77e35afe752f01663dd5c0
David Kallesen <david@lmdk.dk>
2024-05-21 19:46:20 +0200

    test: Simple update og test on atc-nuget-package to 2.0.472

--- test/Atc.Rest.ApiGenerator.Nuget.Tests/Clients/AtcApiNugetClientIntegrationTests.cs
+++ test/Atc.Rest.ApiGenerator.Nuget.Tests/Clients/AtcApiNugetClientIntegrationTests.cs
@@ -5,7 +5,7 @@ namespace Atc.Rest.ApiGenerator.Nuget.Tests.Clients;
 public class AtcApiNugetClientIntegrationTests
 {
     [Theory]
-    [InlineData("2.0.280", "Atc")]
+    [InlineData("2.0.472", "Atc")]
     [InlineData(null, "xAtcDummy")]
     public async Task RetrieveLatestVersionForPackageId(
         string? expectedMinPackageVersion,
