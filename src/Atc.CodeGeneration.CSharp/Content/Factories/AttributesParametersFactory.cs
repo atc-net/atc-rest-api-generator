ff23270483e9a8d8a03c524de860cc520f6cdbb3
David Kallesen <david@lmdk.dk>
2024-07-15 01:10:19 +0200

    feat: Improve RangeAttribute content for Min & Max dotnet types

--- src/Atc.CodeGeneration.CSharp/Content/Factories/AttributesParametersFactory.cs
+++ src/Atc.CodeGeneration.CSharp/Content/Factories/AttributesParametersFactory.cs
@@ -38,7 +38,49 @@ public static class AttributesParametersFactory
                     result.Add(new AttributeParameters("MaxLength", maxLengthAttribute.Length.ToString()));
                     break;
                 case RangeAttribute rangeAttribute:
-                    result.Add(new AttributeParameters("Range", $"{rangeAttribute.Minimum}, {rangeAttribute.Maximum}"));
+                    var minimum = rangeAttribute.Minimum;
+                    var maximum = rangeAttribute.Maximum;
+
+                    if (rangeAttribute.OperandType == typeof(double))
+                    {
+                        if (double.MinValue.Equals((double)rangeAttribute.Minimum) ||
+                            rangeAttribute.Minimum is double.NegativeInfinity)
+                        {
+                            minimum = "double.MinValue";
+                        }
+
+                        if (double.MaxValue.Equals((double)rangeAttribute.Maximum) ||
+                            rangeAttribute.Maximum is double.PositiveInfinity)
+                        {
+                            maximum = "double.MaxValue";
+                        }
+                    }
+                    else if (rangeAttribute.OperandType == typeof(int))
+                    {
+                        if (int.MinValue.Equals((int)rangeAttribute.Minimum))
+                        {
+                            minimum = "int.MinValue";
+                        }
+
+                        if (int.MaxValue.Equals((int)rangeAttribute.Maximum))
+                        {
+                            maximum = "int.MaxValue";
+                        }
+                    }
+                    else if (rangeAttribute.OperandType == typeof(long))
+                    {
+                        if (long.MinValue.Equals((long)rangeAttribute.Minimum))
+                        {
+                            minimum = "long.MinValue";
+                        }
+
+                        if (long.MaxValue.Equals((long)rangeAttribute.Maximum))
+                        {
+                            maximum = "long.MaxValue";
+                        }
+                    }
+
+                    result.Add(new AttributeParameters("Range", $"{minimum}, {maximum}"));
                     break;
                 case RegularExpressionAttribute regularExpressionAttribute:
                     result.Add(new AttributeParameters("RegularExpression", regularExpressionAttribute.GetEscapedPattern()));
