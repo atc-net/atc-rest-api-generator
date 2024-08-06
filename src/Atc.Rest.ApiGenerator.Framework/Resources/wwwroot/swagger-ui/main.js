26947104695edbde4f097c3bf3d09c917c4795fd
David Kallesen <david@lmdk.dk>
2024-08-06 14:23:18 +0200

    fix: remove old swagger icon rendered in the dom by removing a link tag

--- src/Atc.Rest.ApiGenerator.Framework/Resources/wwwroot/swagger-ui/main.js
+++ src/Atc.Rest.ApiGenerator.Framework/Resources/wwwroot/swagger-ui/main.js
@@ -1,5 +1,18 @@
-﻿var link = document.querySelector("link[rel*='icon']") || document.createElement("link");
+var link = document.querySelector("link[rel*='icon']") || document.createElement("link");
 link.type = "image/x-icon";
 link.rel = "shortcut icon";
 link.href = "/favicon.ico";
 document.getElementsByTagName("head")[0].appendChild(link);
+
+document.addEventListener('DOMContentLoaded', function () {
+  setTimeout(function () {
+    const linkElement = document.querySelector('div.topbar-wrapper > a[rel="noopener noreferrer"].link');
+    if (linkElement) {
+      const svgElement = linkElement.querySelector('svg');
+      if (svgElement) {
+        linkElement.parentNode.insertBefore(svgElement, linkElement);
+      }
+      linkElement.remove();
+    }
+  }, 100);
+}, false);
