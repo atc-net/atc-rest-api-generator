2132159c9177fe852fa3e40d58201bfd88f35d56
David Kallesen <david@lmdk.dk>
2024-12-22 00:27:00 +0100

    feat: add NamespaceFactory

---
+++ src/Atc.Rest.ApiGenerator.Framework/Factories/NamespaceFactory.cs
@@ -0,0 +1,21 @@
+namespace Atc.Rest.ApiGenerator.Framework.Factories;
+
+public static class NamespaceFactory
+{
+    public static string CreateFull(
+        string projectName,
+        params string[] subNamespaces)
+    {
+        var fullNamespace = string.Join(' ', new[] { projectName }.Concat(subNamespaces));
+
+        return Create(fullNamespace);
+    }
+
+    public static string Create(
+        params string[] subNamespaces)
+    {
+        var fullNamespace = string.Join(' ', subNamespaces);
+
+        return fullNamespace.EnsureNamespaceFormat();
+    }
+}
