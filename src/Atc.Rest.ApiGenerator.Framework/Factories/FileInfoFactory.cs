a7956663fcfa135e3f8d3d22c9f3447e342af634
David Kallesen <david@lmdk.dk>
2024-12-22 00:27:12 +0100

    feat: add FileInfoFactory

---
+++ src/Atc.Rest.ApiGenerator.Framework/Factories/FileInfoFactory.cs
@@ -0,0 +1,32 @@
+namespace Atc.Rest.ApiGenerator.Framework.Factories;
+
+public static class FileInfoFactory
+{
+    public static FileInfo Create(
+        DirectoryInfo projectPath,
+        params string[] subParts)
+    {
+        if (subParts is null || subParts.Length == 0)
+        {
+            throw new ArgumentException("At least one path part must be provided.", nameof(subParts));
+        }
+
+        var fileName = subParts[^1];
+
+        if (!fileName.Contains('.', StringComparison.Ordinal))
+        {
+            throw new ArgumentException("The file name must include a valid extension (e.g., .cs).", nameof(subParts));
+        }
+
+        var pathParts = subParts[..^1];
+
+        var subPath = string
+            .Join('.', pathParts)
+            .Trim()
+            .Replace('\\', ' ')
+            .Replace('-', ' ')
+            .Replace('.', ' ')
+            .PascalCase(separators: [' ', '-']);
+        return projectPath.CombineFileInfo(subPath.Split(' ').Concat([fileName]).ToArray());
+    }
+}
