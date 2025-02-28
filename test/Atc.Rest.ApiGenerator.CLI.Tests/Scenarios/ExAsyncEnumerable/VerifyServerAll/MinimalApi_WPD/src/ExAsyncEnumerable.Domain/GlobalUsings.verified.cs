ef97a622c8dd587b5504fa14dc8145ce44cb39b9
Per Kops <pko@delegate.dk>
2025-02-28 14:47:21 +0100

    test: update scenario tests

--- test/Atc.Rest.ApiGenerator.CLI.Tests/Scenarios/ExAsyncEnumerable/VerifyServerAll/MinimalApi_WPD/src/ExAsyncEnumerable.Domain/GlobalUsings.verified.cs
+++ test/Atc.Rest.ApiGenerator.CLI.Tests/Scenarios/ExAsyncEnumerable/VerifyServerAll/MinimalApi_WPD/src/ExAsyncEnumerable.Domain/GlobalUsings.verified.cs
@@ -3,6 +3,8 @@ global using System.Diagnostics.CodeAnalysis;
 
 global using ExAsyncEnumerable.Api.Generated.Contracts.Customers;
 global using ExAsyncEnumerable.Api.Generated.Contracts.Users;
+global using ExAsyncEnumerable.Domain.Handlers.Customers;
+global using ExAsyncEnumerable.Domain.Handlers.Users;
 
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
