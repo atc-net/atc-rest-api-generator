a44f12d5ffe9afef7da9aae064049ef52a107c00
David Kallesen <david@lmdk.dk>
2025-03-01 01:29:47 +0100

    feat: add PrivateStaticAsync

--- src/Atc.CodeGeneration.CSharp/Content/DeclarationModifiers.cs
+++ src/Atc.CodeGeneration.CSharp/Content/DeclarationModifiers.cs
@@ -55,6 +55,9 @@ public enum DeclarationModifiers
     [Description("private async")]
     PrivateAsync,
 
+    [Description("private static async")]
+    PrivateStaticAsync,
+
     [Description("protected")]
     Protected,
 
